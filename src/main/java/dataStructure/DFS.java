adf4d801d75ef39a04e4c789b24754b495a087df
김승훈 <dolnamool564@mumun.co.kr>
2021-09-27 22:57:03 +0900

    firstCommit

---
+++ src/main/java/dataStructure/DFS.java
@@ -0,0 +1,28 @@
+package dataStructure;
+
+import java.util.LinkedList;
+
+class Graph{
+    class Node{
+        int data;
+        LinkedList<Node> adjecent;
+        boolean marked;
+    }
+
+    void addEdge(int i1, int i2){
+
+    }
+}
+
+public class DFS {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        System.out.println(stack.);
+
+    }
+}
