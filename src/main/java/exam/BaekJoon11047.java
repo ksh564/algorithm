5b2cd4aef7a575412599c172dc5ea8a083ea4db1
dolnamool564 <dolnamool564@mumun.co.kr>
2021-10-05 16:53:06 +0900

    BaekJoon11047

---
+++ src/main/java/exam/BaekJoon11047.java
@@ -0,0 +1,27 @@
+package exam;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BaekJoon11047 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count  = scanner.nextInt();
+        int total  = scanner.nextInt();
+        int result = 0;
+
+        Integer[] arr = new Integer[count];
+
+        for(int i=0; i<count; i++){
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(int j=0; j<count; j++){
+            result += total / arr[j];
+            total %= arr[j];
+        }
+        System.out.println(result);
+    }
+}
