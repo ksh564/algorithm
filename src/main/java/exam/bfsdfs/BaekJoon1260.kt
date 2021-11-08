2acccf5a1043fa1ab51fb681b902e0863d193c4e
dolnamool564 <23445841+ksh564@users.noreply.github.com>
2021-11-08 16:31:05 +0900

    백준 1389

---
+++ src/main/java/exam/bfsdfs/BaekJoon1260.kt
@@ -0,0 +1,62 @@
+package exam.bfsdfs
+import java.util.*
+
+object BaekJoon1260 {
+    lateinit var visited: BooleanArray
+    lateinit var maps: Array<IntArray>
+    var N = 0
+    var M = 0
+    var V = 0
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val scanner = Scanner(System.`in`)
+        N = scanner.nextInt()
+        M = scanner.nextInt()
+        V = scanner.nextInt()
+        visited = BooleanArray(N + 1)
+        maps = Array(N + 1) { IntArray(N + 1) }
+        for (i in 1..M) {
+            val start = scanner.nextInt()
+            val arrived = scanner.nextInt()
+            maps[start][arrived] = 1
+            maps[arrived][start] = 1
+        }
+        println("DFS Start")
+        dfs(V)
+        visited = BooleanArray(N + 1)
+        println("BFS Start")
+        bfs(V)
+    }
+
+    private fun dfs(point: Int) {
+        val stack: Stack<Int> = Stack<Int>()
+        stack.push(point)
+        while (!stack.isEmpty()) {
+            val index = stack.pop() as Int
+            if (visited[index]) continue
+            visited[index] = true
+            print("$index->")
+            for (i in N downTo 1) {
+                if (maps[index][i] == 1 && !visited[i]) {
+                    stack.push(i)
+                }
+            }
+        }
+    }
+
+    private fun bfs(point: Int) {
+        val queue: Queue<Int> = LinkedList()
+        queue.offer(point)
+        while (!queue.isEmpty()) {
+            val index = queue.poll()
+            visited[index] = true
+            print("$index->")
+            for (i in 1..N) {
+                if (maps[index][i] == 1 && !visited[i]) {
+                    queue.offer(i)
+                    visited[i] = true
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/exam/dijkstra/BaekJoon1389.java
@@ -0,0 +1,56 @@
+package exam.dijkstra;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon1389 {
+    private static int N,M;
+    private static int [][] relations;
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+
+        relations = new int[N+1][N+1];
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(i!=j) relations[i][j] = 99999;
+            }
+        }
+        for(int i=1; i<=N; i++){
+            int start = scanner.nextInt();
+            int end = scanner.nextInt();
+            relations[start][end] = 1;
+            relations[end][start] = 1;
+        }
+        floyd();
+        int min = Integer.MAX_VALUE;
+        int minIndex = 1;
+        int[] res = new int[N+1];
+        for(int i=1; i<=N; i++){
+            int sum = 0;
+            for(int j=1; j<=N; j++){
+                sum += relations[i][j];
+            }
+            res[i] = sum;
+            if(min > sum){
+                min = sum;
+                minIndex = i;
+            }
+        }
+        System.out.println(minIndex);
+    }
+
+    public static void floyd(){
+        for(int k=1; k<=N; k++){
+            for(int i=1; i<=N; i++){
+                for(int j=1; j<=N; j++){
+                    relations[i][j] = Math.min(relations[i][j],relations[i][k]+relations[k][j]);
+                }
+            }
+        }
+    }
+
+
+}
