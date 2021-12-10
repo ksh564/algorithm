b92ce2e95c8f43e14a5cc0c5120e03907634d912
김승훈 <23445841+ksh564@users.noreply.github.com>
2021-12-10 22:18:21 +0900

    12.10

---
+++ src/main/java/codility/Codility06.java
@@ -0,0 +1,22 @@
+package codility;
+
+import java.util.Arrays;
+
+public class Codility06 {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        if(A.length <= 2){
+            return 0;
+        }
+        Arrays.sort(A);
+        for(int i=0; i<A.length-2; i++){
+            if((long)A[i]+A[i+1] > A[i+2]){
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
